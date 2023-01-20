74fc51efde596c126ec36b7a6492335ef3bc278e
NikitaSyrtsev <99359976+NikitaSyrtsev@users.noreply.github.com>
2023-01-21 01:05:35 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+let textarea = document.getElementById("textarea");
+
+let btnUp = (document.getElementById("upper-case").onclick = function () {
+  textarea.value = textarea.value.toUpperCase();
+});
+
+let btnLo = (document.getElementById("lower-case").onclick = function () {
+  textarea.value = textarea.value.toLowerCase();
+});
+
+let btnPr = (document.getElementById("proper-case").onclick = function () {
+  let stre = textarea.value.split(" ");
+  for (let i = 0; i < stre.length; i++) {
+    stre[i] = stre[i].charAt(0).toUpperCase() + stre[i].slice(1);
+  }
+  textarea.value = stre.join(" ");
+});
+
+let btnSe = (document.getElementById("sentence-case").onclick = function () {
+  let stre = textarea.value.split(". ");
+  for (let i = 0; i < stre.length; i++) {
+    stre[i] = stre[i].charAt(0).toUpperCase() + stre[i].slice(1).toLowerCase();
+  }
+  textarea.value = stre.join(". ");
+});
+
+function download(filename, text) {
+  let element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+document.getElementById("save-text-file").addEventListener(
+  "click",
+  function () {
+    let text = document.getElementById("text").value;
+    let filename = "text.txt";
+    download(filename, text);
+  },
+  false
+);
